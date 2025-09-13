e5ca877a19d9eee0a555b769ed90cc52613c19bb
Sakshi-G-codes <sakshi31692@gmail.com>
2025-09-14 00:11:36 +0530

    small beginnig

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <h1>Welcome to the vital Nest</h1>
+      <p>this is very beginning to the react</p>
+      <button>hello react</button>
     </div>
   );
 }
